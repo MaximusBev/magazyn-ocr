11284c4cfbd03b4803b1eb8d953034ce42ff81a5
Maks <maks.seluygin@gmail.com>
2025-04-05 12:10:05 +0200

    Оновлення сайту

--- app.py
+++ app.py
@@ -196,4 +196,6 @@ def delete_all_images(firm_name):
     return redirect(url_for('view_firm', firm_name=firm_name))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
